ca7b7b560f15ee887735c30992fba9487343cd0a
woojink <woojink01@gmail.com>
2016-08-25 09:25:12 +0100

    Tabs to spaces

--- api/DBpediaExtractor.py
+++ api/DBpediaExtractor.py
@@ -3,132 +3,132 @@ import rdflib
 BASE_URL = 'http://dbpedia.org/resource/'
 
 class DBpediaExtractor(object):
-	"""
-	Extractor for DBPedia
+    """
+    Extractor for DBPedia
 
-	Attributes:
-		entity (str): URL friendly name for the entity
-		url (str): The main resource URL
-		g (rdflib graph): DBPedia RDF graph object
-	"""
+    Attributes:
+        entity (str): URL friendly name for the entity
+        url (str): The main resource URL
+        g (rdflib graph): DBPedia RDF graph object
+    """
 
-	def __init__(self, url):
-		"""
-		Args:
-			url (str): Raw DBPedia URL
-		"""
-		self.entity = url.split("http://dbpedia.org/resource/")[-1]
-		self.url = BASE_URL + self.entity
-		self.g = self.get_rdf()
-	
-	# Internal functions
-	def get_rdf(self):
-		g = rdflib.Graph()
-		g.load(self.url)
-		return g
+    def __init__(self, url):
+        """
+        Args:
+            url (str): Raw DBPedia URL
+        """
+        self.entity = url.split("http://dbpedia.org/resource/")[-1]
+        self.url = BASE_URL + self.entity
+        self.g = self.get_rdf()
+    
+    # Internal functions
+    def get_rdf(self):
+        g = rdflib.Graph()
+        g.load(self.url)
+        return g
 
-	def get_ontology(self, endpoint):
-		"""
-		Returns ontology information for the given endpoint
+    def get_ontology(self, endpoint):
+        """
+        Returns ontology information for the given endpoint
 
-		Args:
-			endpoint (url): Endpoint for the desired ontology
-		"""
-		r_list = []
-		for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/ontology/" + endpoint)):
-			r_list.append(str(stmt[1]))
-		return r_list
+        Args:
+            endpoint (url): Endpoint for the desired ontology
+        """
+        r_list = []
+        for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/ontology/" + endpoint)):
+            r_list.append(str(stmt[1]))
+        return r_list
 
-	def get_properties(self, endpoint, subject=False):
-		"""
-		Returns properties for the given endpoint
+    def get_properties(self, endpoint, subject=False):
+        """
+        Returns properties for the given endpoint
 
-		Args:
-			endpoint (str): Endpoint for the desired properties
-		"""
-		r_list = []
-		for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/property/" + endpoint)):
-			if subject:
-				r_list.append(str(stmt[0]))
-			else:
-				r_list.append(str(stmt[1]))
-		return r_list
+        Args:
+            endpoint (str): Endpoint for the desired properties
+        """
+        r_list = []
+        for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/property/" + endpoint)):
+            if subject:
+                r_list.append(str(stmt[0]))
+            else:
+                r_list.append(str(stmt[1]))
+        return r_list
 
-	def get_geo(self, endpoint):
-		r_list = []
-		for stmt in self.g.subject_objects(rdflib.URIRef("http://www.w3.org/2003/01/geo/wgs84_pos#" + endpoint)):
-			r_list.append(float(stmt[1]))
-		return r_list
+    def get_geo(self, endpoint):
+        r_list = []
+        for stmt in self.g.subject_objects(rdflib.URIRef("http://www.w3.org/2003/01/geo/wgs84_pos#" + endpoint)):
+            r_list.append(float(stmt[1]))
+        return r_list
 
-	def get_misc(self, endpoint, lang=None):
-		r_list = []
-		if lang:
-			for stmt in self.g.subject_objects(rdflib.URIRef(endpoint)):
-				if stmt[1].language == lang:
-					r_list.append(str(stmt[1]))
-		else:
-			for stmt in self.g.subject_objects(rdflib.URIRef(endpoint)):
-				r_list.append(str(stmt[1]))
-		return r_list
+    def get_misc(self, endpoint, lang=None):
+        r_list = []
+        if lang:
+            for stmt in self.g.subject_objects(rdflib.URIRef(endpoint)):
+                if stmt[1].language == lang:
+                    r_list.append(str(stmt[1]))
+        else:
+            for stmt in self.g.subject_objects(rdflib.URIRef(endpoint)):
+                r_list.append(str(stmt[1]))
+        return r_list
 
-	# General
-	def get_name(self):
-		return self.get_properties("name")
+    # General
+    def get_name(self):
+        return self.get_properties("name")
 
-	def get_label(self):
-		return str(self.g.preferredLabel(rdflib.term.URIRef(self.url), lang="en")[0][1])
+    def get_label(self):
+        return str(self.g.preferredLabel(rdflib.term.URIRef(self.url), lang="en")[0][1])
 
-	def get_comment(self):
-		return self.get_misc("http://www.w3.org/2000/01/rdf-schema#comment", lang="en")
+    def get_comment(self):
+        return self.get_misc("http://www.w3.org/2000/01/rdf-schema#comment", lang="en")
 
-	# People
-	def get_birthDate(self):
-		return self.get_ontology("birthDate")
+    # People
+    def get_birthDate(self):
+        return self.get_ontology("birthDate")
 
-	def get_deathDate(self):
-		return self.get_ontology("deathDate")
+    def get_deathDate(self):
+        return self.get_ontology("deathDate")
 
-	def get_birthPlace_uri(self):
-		return self.get_ontology("birthPlace")
+    def get_birthPlace_uri(self):
+        return self.get_ontology("birthPlace")
 
-	def get_deathPlace_uri(self):
-		return self.get_ontology("deathPlace")
+    def get_deathPlace_uri(self):
+        return self.get_ontology("deathPlace")
 
-	def get_residence(self):
-		return self.get_ontology("residence")
+    def get_residence(self):
+        return self.get_ontology("residence")
 
-	def get_knownFor(self):
-		return self.get_ontology("knownFor")
+    def get_knownFor(self):
+        return self.get_ontology("knownFor")
 
-	def get_almaMater(self):
-		return self.get_ontology("almaMater")
+    def get_almaMater(self):
+        return self.get_ontology("almaMater")
 
-	def get_spouse(self):
-		r_list = []
-		for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/ontology/" + "spouse")):
-			# Filters through to exclude symmetrical (repeated) information
-			if str(stmt[1]) != self.url:
-				r_list.append(str(stmt[1]))
-		return r_list
+    def get_spouse(self):
+        r_list = []
+        for stmt in self.g.subject_objects(rdflib.URIRef("http://dbpedia.org/ontology/" + "spouse")):
+            # Filters through to exclude symmetrical (repeated) information
+            if str(stmt[1]) != self.url:
+                r_list.append(str(stmt[1]))
+        return r_list
 
-	def get_description(self):
-		return self.get_misc("http://purl.org/dc/elements/1.1/description", lang="en")
+    def get_description(self):
+        return self.get_misc("http://purl.org/dc/elements/1.1/description", lang="en")
 
-	def get_mp(self):
-		return self.get_properties("mp", subject=True)
+    def get_mp(self):
+        return self.get_properties("mp", subject=True)
 
-	# Locations
-	def get_coordinates(self):
-		"""
-		Returns:
-			(lat, lng): Tuple of latitude, longitude coordinates
-		"""
-		lat = self.get_geo('lat')
-		lng = self.get_geo('long')
-		return list(zip(lat, lng))
+    # Locations
+    def get_coordinates(self):
+        """
+        Returns:
+            (lat, lng): Tuple of latitude, longitude coordinates
+        """
+        lat = self.get_geo('lat')
+        lng = self.get_geo('long')
+        return list(zip(lat, lng))
 
-	def get_latitude(self):
-		return self.get_geo('lat')
+    def get_latitude(self):
+        return self.get_geo('lat')
 
-	def get_longitude(self):
-		return self.get_geo('long')
+    def get_longitude(self):
+        return self.get_geo('long')
--- api/get_enriched_entities.py
+++ api/get_enriched_entities.py
@@ -3,81 +3,81 @@ from .DBpediaExtractor import DBpediaExtractor
 import json
 
 def get_enriched_entities(text):
-	ee = AlchemyExtractor(text)
-	return_dict = []
+    ee = AlchemyExtractor(text)
+    return_dict = []
 
-	place_list = ee.get_places()
-	for (alchemy_label, uri, rel) in place_list:
-		return_dict.append(get_place_dict(alchemy_label, uri, rel))
+    place_list = ee.get_places()
+    for (alchemy_label, uri, rel) in place_list:
+        return_dict.append(get_place_dict(alchemy_label, uri, rel))
 
-	name_list = ee.get_names()
-	for (alchemy_label, uri, rel) in name_list:
-		return_dict.append(get_name_dict(alchemy_label, uri, rel))
+    name_list = ee.get_names()
+    for (alchemy_label, uri, rel) in name_list:
+        return_dict.append(get_name_dict(alchemy_label, uri, rel))
 
-	return json.dumps(return_dict, indent=2, sort_keys=True)
+    return json.dumps(return_dict, indent=2, sort_keys=True)
 
 def get_place_dict(alchemy_label, uri, rel):
-	t_dict = {}
-	dbe = DBpediaExtractor(uri)
+    t_dict = {}
+    dbe = DBpediaExtractor(uri)
 
-	t_dict['uri'] = uri
-	t_dict['type'] = 'place'
-	t_dict['label'] = dbe.get_label()
-	t_dict['relevance_score'] = rel
-	t_dict['alchemy_label'] = alchemy_label
+    t_dict['uri'] = uri
+    t_dict['type'] = 'place'
+    t_dict['label'] = dbe.get_label()
+    t_dict['relevance_score'] = rel
+    t_dict['alchemy_label'] = alchemy_label
 
-	t_dict['latitude'] = dbe.get_latitude()
-	t_dict['longitude'] = dbe.get_longitude()
-	t_dict['comment'] = dbe.get_comment()
-	t_dict['coordinates'] = dbe.get_coordinates()
+    t_dict['latitude'] = dbe.get_latitude()
+    t_dict['longitude'] = dbe.get_longitude()
+    t_dict['comment'] = dbe.get_comment()
+    t_dict['coordinates'] = dbe.get_coordinates()
 
-	return t_dict
+    return t_dict
 
 def get_name_dict(alchemy_label, uri, rel, expand=True):
-	t_dict = {}
-	dbe = DBpediaExtractor(uri)
+    t_dict = {}
+    dbe = DBpediaExtractor(uri)
 
-	t_dict['uri'] = uri
-	t_dict['type'] = 'person'
-	t_dict['label'] = dbe.get_label()
+    t_dict['uri'] = uri
+    t_dict['type'] = 'person'
+    t_dict['label'] = dbe.get_label()
 
-	if rel:
-		t_dict['relevance_score'] = rel
-	if alchemy_label:
-		t_dict['alchemy_label'] = alchemy_label
+    if rel:
+        t_dict['relevance_score'] = rel
+    if alchemy_label:
+        t_dict['alchemy_label'] = alchemy_label
 
-	t_dict['alma_mater'] = expand_place(dbe.get_almaMater())
+    t_dict['alma_mater'] = expand_place(dbe.get_almaMater())
 
-	t_dict['birth_date'] = dbe.get_birthDate()
-	t_dict['birthplace_uri'] = expand_place(dbe.get_birthPlace_uri())
-	t_dict['death_date'] = dbe.get_deathDate()	
-	t_dict['deathplace_uri'] = expand_place(dbe.get_deathPlace_uri())
-	t_dict['description'] = dbe.get_description()
-	t_dict['known_for'] = dbe.get_knownFor()
-	t_dict['residence'] = expand_place(dbe.get_residence())
-	if expand:
-		t_dict['spouse'] = expand_person(dbe.get_spouse())
-	else:
-		t_dict['spouse'] = dbe.get_spouse()
+    t_dict['birth_date'] = dbe.get_birthDate()
+    t_dict['birthplace_uri'] = expand_place(dbe.get_birthPlace_uri())
+    t_dict['death_date'] = dbe.get_deathDate()  
+    t_dict['deathplace_uri'] = expand_place(dbe.get_deathPlace_uri())
+    t_dict['description'] = dbe.get_description()
+    t_dict['known_for'] = dbe.get_knownFor()
+    t_dict['residence'] = expand_place(dbe.get_residence())
+    if expand:
+        t_dict['spouse'] = expand_person(dbe.get_spouse())
+    else:
+        t_dict['spouse'] = dbe.get_spouse()
 
-	return t_dict
+    return t_dict
 
 def expand_place(place_list):
-	temp_list = []
-	if place_list:
-		for subplace in place_list:
-			if "http://dbpedia.org/resource/" in subplace:
-				temp_list.append(get_place_dict(None, subplace, None))
-			else:
-				temp_list.append({'label': subplace})
-	return temp_list
+    temp_list = []
+    if place_list:
+        for subplace in place_list:
+            if "http://dbpedia.org/resource/" in subplace:
+                temp_list.append(get_place_dict(None, subplace, None))
+            else:
+                temp_list.append({'label': subplace})
+    return temp_list
 
 def expand_person(name_list):
-	temp_list = []
-	if name_list:
-		for subperson in name_list:
-			if "http://dbpedia.org/resource/" in subperson:
-				temp_list.append(get_name_dict(None, subperson, None, expand=False))
-			else:
-				temp_list.append({'label': subperson})
-	return temp_list
+    temp_list = []
+    if name_list:
+        for subperson in name_list:
+            if "http://dbpedia.org/resource/" in subperson:
+                temp_list.append(get_name_dict(None, subperson, None, expand=False))
+            else:
+                temp_list.append({'label': subperson})
+    return temp_list
