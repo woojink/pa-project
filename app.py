b57c7778408d515e78a21eb0419a53d38102136e
woojink <woojink01@gmail.com>
2016-08-26 14:46:25 +0100

    Formatting

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ app = Flask(__name__)
 def request_entities():
     if not request.json or not 'headline' in request.json:
         abort(400)
-    body_splitted = str.split(request.json['body_text'] ,'\n\n')
 
+    body_splitted = str.split(request.json['body_text'] ,'\n\n')
     body_3_first_sent = body_splitted[:min(len(body_splitted),3)]
-    
     text = request.json['headline'] + request.json['description_text'] + '. '.join(body_3_first_sent)
-    
-    response = get_enriched_entities(text)
-    resp = Response(response=response, status=200, mimetype="application/json")
-   
+
+    resp = Response(response=get_enriched_entities(text), status=200, mimetype="application/json")
+
     return(resp)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-#
